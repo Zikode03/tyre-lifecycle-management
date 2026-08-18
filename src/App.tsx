378021b07529949a4f957bb8532c6b0eb195c137
Zikode03 <ngcebonkwanyana03@gmail.com>
2026-08-18 09:11:58 +0200

    Route job cards and quotation workflow

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,8 @@ import FleetOperationsPage from './pages/FleetOperationsPage';
 import InspectionWorkspacePage from './pages/InspectionWorkspacePage';
 import InspectionsPage from './pages/InspectionsPage';
 import IntegrationsPage from './pages/IntegrationsPage';
+import JobCardsPage from './pages/JobCardsPage';
+import JobCardWorkspacePage from './pages/JobCardWorkspacePage';
 import LifecycleCentrePage from './pages/LifecycleCentrePage';
 import LoginPage from './pages/LoginPage';
 import NotificationsPage from './pages/NotificationsPage';
@@ -35,6 +37,7 @@ export default function App(){return <Routes>
   <Route path="/vehicles" element={<VehiclesPage/>}/><Route path="/vehicles/:vehicleId" element={<VehicleWorkspacePage/>}/>
   <Route path="/tyres" element={<TyresPage/>}/><Route path="/tyres/fitment/new" element={<TyreFitmentPage/>}/><Route path="/tyres/:tyreId" element={<TyrePassportPage/>}/>
   <Route path="/inspections" element={<InspectionsPage/>}/><Route path="/inspections/vehicle/new" element={<VehicleInspectionPage/>}/><Route path="/inspections/:inspectionId" element={<InspectionWorkspacePage/>}/>
+  <Route path="/job-cards" element={<JobCardsPage/>}/><Route path="/job-cards/:jobCardId" element={<JobCardWorkspacePage/>}/>
   <Route path="/scan-review" element={<ScanReviewPage/>}/><Route path="/devices-tpms" element={<DevicesTpmsPage/>}/><Route path="/fleet" element={<FleetOperationsPage/>}/>
   <Route path="/bookings" element={<BookingsPage/>}/><Route path="/bookings/:bookingId" element={<BookingWorkspacePage/>}/>
   <Route path="/warranty" element={<WarrantyPage/>}/><Route path="/warranty/:claimId" element={<WarrantyClaimPage/>}/><Route path="/notifications" element={<NotificationsPage/>}/><Route path="/reports" element={<ReportsPage/>}/><Route path="/integrations" element={<IntegrationsPage/>}/><Route path="/settings" element={<SettingsPage/>}/>
